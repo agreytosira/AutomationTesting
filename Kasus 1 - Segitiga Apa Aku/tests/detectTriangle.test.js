4f35e024a495049ebab26547e1d16a11429c6952
Agrey Tosira <agreytosira@gmail.com>
2023-10-28 20:16:48 +0700

    fix eslint and add inequality principle function

--- Kasus 1 - Segitiga Apa Aku/tests/detectTriangle.test.js
+++ Kasus 1 - Segitiga Apa Aku/tests/detectTriangle.test.js
@@ -12,6 +12,10 @@ const detectTriangle = (sideA, sideB, sideC) => {
     })
     .sort()
 
+  if (sideA + sideB <= sideC) {
+    throw new Error('Triangle does not conform inequality principle')
+  }
+
   if (sideA === sideB && sideA === sideC) {
     return 'Equilateral Triangle'
   }
@@ -30,6 +34,11 @@ describe('Detect the triangle', () => {
     expect(() => detectTriangle(1, 2, 'a')).toThrowError('Sides have to be Integer')
   })
 
+  it('Should detect inequality problem', () => {
+    expect(() => detectTriangle(4, 1, 2)).toThrowError('Triangle does not conform inequality principle')
+    expect(() => detectTriangle(5, 1, 3)).toThrowError('Triangle does not conform inequality principle')
+  })
+
   it('Should fail if the sides are less than 1', () => {
     expect(() => detectTriangle(-1, 2, 2)).toThrowError('Strange Triangle')
     expect(() => detectTriangle(1, -2, 2)).toThrowError('Strange Triangle')
@@ -42,14 +51,14 @@ describe('Detect the triangle', () => {
   })
 
   it('Should detect Isosceles Triangle if only two sides are equal', () => {
-    expect(detectTriangle(1, 1, 2)).toEqual('Isosceles Triangle')
-    expect(detectTriangle(1, 2, 1)).toEqual('Isosceles Triangle')
+    expect(detectTriangle(2, 2, 3)).toEqual('Isosceles Triangle')
+    expect(detectTriangle(4, 2, 4)).toEqual('Isosceles Triangle')
     expect(detectTriangle(1, 2, 2)).toEqual('Isosceles Triangle')
 
     expect(detectTriangle(4, 1, 4)).toEqual('Isosceles Triangle')
   })
 
   it('Should detect Scalene Triangle if no sides are the same', () => {
-    expect(detectTriangle(1, 2, 3)).toEqual('Scalene Triangle')
+    expect(detectTriangle(2, 4, 3)).toEqual('Scalene Triangle')
   })
 })
